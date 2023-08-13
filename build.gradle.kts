822a10cce6afe1dd4f4e40a071fa501293a5b39d
Maia Grotepass <maiatoday@gmail.com>
2023-08-13 08:44:47 +0200

    funny fix to get it to run from the gutter

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,31 @@ kotlin {
         }
         val jvmTest by getting
     }
+    val osName = System.getProperty("os.name")
+    val targetOs = when {
+        osName == "Mac OS X" -> "macos"
+        osName.startsWith("Win") -> "windows"
+        osName.startsWith("Linux") -> "linux"
+        else -> error("Unsupported OS: $osName")
+    }
+
+    val targetArch = when (val osArch = System.getProperty("os.arch")) {
+        "x86_64", "amd64" -> "x64"
+        "aarch64" -> "arm64"
+        else -> error("Unsupported arch: $osArch")
+    }
+
+    val version = "0.7.74" // or any more recent version
+    val target = "${targetOs}-${targetArch}"
+
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation("org.jetbrains.skiko:skiko-awt-runtime-$target:$version")
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
 }
 
 compose.desktop {
